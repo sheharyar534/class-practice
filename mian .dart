6ccf86ba3f3e9d5b5e820b0429c785fb0fee7756
sheharyar534 <sheharyar.1701188@gmail.com>
2024-03-07 17:13:05 +0500

    Create mian .dart

---
+++ mian .dart
@@ -0,0 +1,10 @@
+
+void main() {
+  // Generate a random number between 1 and 100
+  int randomNumber = Random().nextInt(100) + 1;
+  print('Random number: $randomNumber');
+
+  // Calculate the square root of the random number
+  double squareRoot = sqrt(randomNumber);
+  print('Square root of the random number: $squareRoot');
+}
